a8a981cb9c2d1a57846fa854e325fb9d85aaa4d5
Mauricio Mikulski <incognitomikulski@gmail.com>
2022-04-07 22:26:58 -0300

    remove coverage

--- source/interfaces/IUser.ts
+++ source/interfaces/IUser.ts
@@ -2,4 +2,5 @@ export default interface IUser {
   _id: number;
   user_login: string;
   user_password: string;
+  user_tipo: number;
 }
--- source/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Users {
-  id?: string,
-  nome?: string,
-  login?: string,
-  senha?: string
-}
